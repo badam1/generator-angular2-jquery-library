9d6380d377bbf551e1c9840045f309ce2bc16eba
bodansky <adam.bodansky@gmail.com>
2017-09-13 14:01:09 +0200

    fix for new rollup release

--- generators/app/templates/src/sample.component.ts
+++ generators/app/templates/src/sample.component.ts
@@ -1,6 +1,11 @@
-/// <reference types="jquery" />
 import { Component } from '@angular/core';
+import moment from 'moment';
+
 // add imports here for jquery related js libs
+
+
+import * as jqueryProxy from 'jquery';
+const $: JQueryStatic = (<any>jqueryProxy).default || jqueryProxy;
 
 @Component({
   selector: 'sample-component',
